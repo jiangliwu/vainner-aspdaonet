b0c3025357d8215ae5bf8edb1389ca83651a5441
Vainner <jiangliwu.ipple@gmail.com>
2013-05-05 13:51:31 +0800

    添加了99乘法表

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,7 +14,20 @@ public partial class _Default : System.Web.UI.Page
 
     public void showHelloWorld()
     {
-        Response.Write("<h1>Vainner</h1>");
-        Response.Write("Hello World ! <br/>");
+        Response.Write("<h1>Hello, World!</h1>");
+        Response.Write(" <p>这是一个学习用的简单网站. 基于asp.net和bootstrap，简单构建起一个漂亮的网站，让你在起步中获得快速的成就感!<br/>");
+    }
+
+    public void showNineTable()
+    {
+        Response.Write("</br>9*9乘法表如下:</br></br>");
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Response.Write(i + "*" + j + "=" + j * i + "  ");
+            }
+            Response.Write("</br>");
+        }
     }
 }
